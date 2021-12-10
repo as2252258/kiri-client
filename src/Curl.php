a3c89b419b7d3ceabdf2c63c54a83d70466de879
Administrator <as2252258@163.com>
2021-12-11 05:54:52 +0800

    1

--- src/Curl.php
+++ src/Curl.php
@@ -74,8 +74,8 @@ class Curl extends ClientAbstracts
 	 */
 	private function curlHandlerSslSet(): void
 	{
-        curl_setopt($this->client, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($this->client, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($this->client, CURLOPT_SSL_VERIFYPEER, TRUE);
+        curl_setopt($this->client, CURLOPT_SSL_VERIFYHOST, TRUE);
         if (!empty($this->getSslKeyFile()) && file_exists($this->getSslKeyFile())) {
 			curl_setopt($this->client, CURLOPT_SSLKEY, $this->getSslKeyFile());
 		}
