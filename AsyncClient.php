6565f1d7a3a2c5b6ccb6fa7be905ac7ba353b05d
向林 <as2252258@163.com>
2022-01-11 15:35:59 +0800

    Revert "改名"
    This reverts commit fdf58326

--- AsyncClient.php
+++ AsyncClient.php
@@ -109,7 +109,7 @@ class AsyncClient extends ClientAbstracts
 		$array = $this->_parseHeaders($path);
 
 		$this->client->send(implode("\r\n", $array) . "\r\n\r\n" . $content);
-		$receive = '';
+		$receive = $this->client->recv();
 		while (true) {
 			$tmp = $this->client->recv();
 			if (empty($tmp)) {
